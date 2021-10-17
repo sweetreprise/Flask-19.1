635c2bee5b335c3c863e77b9a5280d7be99d6e21
maria acuna <taeny90@gmail.com>
2021-10-16 21:18:41 -0400

    finished app

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,52 @@
 # Put your app in here.
+from operations import add, sub, mult, div
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/add')
+def calc_add():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    answer = add(a, b)
+
+    return f"{answer}"
+
+@app.route('/sub')
+def calc_sub():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    answer = sub(a, b)
+
+    return f"{answer}"
+
+@app.route('/mult')
+def calc_mult():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    answer = mult(a, b)
+
+    return f"{answer}"
+
+@app.route('/div')
+def calc_div():
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    answer = div(a, b)
+
+    return f"{answer}"
+
+functions = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+}
+
+@app.route('/math/<func>')
+def calc_all(func):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    answer = functions[func](a, b)
+
+    return f"{answer}"
